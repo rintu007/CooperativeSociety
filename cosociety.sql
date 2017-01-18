bde43b635e34e6601e25b7ddadb7f8f4d84fe112
Noor <webmaster.noor@gmail.com>
2017-01-18 15:28:50 +0600

    db

--- cosociety.sql
+++ cosociety.sql
@@ -457,7 +457,7 @@ CREATE TABLE IF NOT EXISTS `members` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8;
 
--- Dumping data for table cosociety.members: ~1 rows (approximately)
+-- Dumping data for table cosociety.members: ~0 rows (approximately)
 DELETE FROM `members`;
 /*!40000 ALTER TABLE `members` DISABLE KEYS */;
 INSERT INTO `members` (`id`, `name`, `MemberCode`, `CSMId`, `BanglaName`, `name12`, `FatherName`, `MotherName`, `HusbandWifeName`, `Age`, `Occupation`, `Nationality`, `NId`, `PassportNo`, `TaxIdNo`, `Phone`, `Mobile`, `PresentVillageName`, `PresentPostOffice`, `PresentUpojela`, `PresentJela`, `SPName`, `SPName2`, `SPFatherName`, `SPFatherName2`, `SPMotherName`, `SPMotherName2`, `SPHusbanWifeName`, `SPHusbanWifeName2`, `Relation`, `Relation2`, `GivenPortion`, `GivenPortion2`, `Image`, `TMSSIdCard`, `NIdImage`, `NomineeImage`, `BirthCertificate`, `NomineeImage2`, `BirthCertificate2`, `created_at`, `updated_at`) VALUES
@@ -1108,12 +1108,41 @@ CREATE TABLE IF NOT EXISTS `savings` (
   FULLTEXT KEY `idx_name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
 
--- Dumping data for table cosociety.savings: ~1 rows (approximately)
+-- Dumping data for table cosociety.savings: ~0 rows (approximately)
 DELETE FROM `savings`;
 /*!40000 ALTER TABLE `savings` DISABLE KEYS */;
 INSERT INTO `savings` (`id`, `name`, `SavingCode`, `unitprice`, `created_at`, `updated_at`) VALUES
 	(37, '1', '2', 3.00, '2017-01-14 04:55:26', '2017-01-14 04:55:26');
 /*!40000 ALTER TABLE `savings` ENABLE KEYS */;
+
+
+-- Dumping structure for table cosociety.shares
+DROP TABLE IF EXISTS `shares`;
+CREATE TABLE IF NOT EXISTS `shares` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(500) DEFAULT NULL,
+  `serial_no` varchar(500) DEFAULT NULL,
+  `member_name` varchar(500) DEFAULT NULL,
+  `member_id` varchar(500) DEFAULT NULL,
+  `mobile_no` varchar(500) DEFAULT NULL,
+  `date12` varchar(500) DEFAULT NULL,
+  `share_number` varchar(500) DEFAULT NULL,
+  `share_amount` varchar(500) DEFAULT NULL,
+  `unitprice` decimal(10,2) DEFAULT '0.00',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table cosociety.shares: ~4 rows (approximately)
+DELETE FROM `shares`;
+/*!40000 ALTER TABLE `shares` DISABLE KEYS */;
+INSERT INTO `shares` (`id`, `name`, `serial_no`, `member_name`, `member_id`, `mobile_no`, `date12`, `share_number`, `share_amount`, `unitprice`, `created_at`, `updated_at`) VALUES
+	(37, NULL, '7', '6', '5', '4', '3', '2', '5', 0.00, '2017-01-18 06:57:31', '2017-01-18 06:57:31'),
+	(38, NULL, '1', '2', '3', '4', '5', '6', '4', 0.00, '2017-01-18 06:57:41', '2017-01-18 06:57:41'),
+	(39, NULL, '7', '6', '5', '4', '3', '2', '5', 0.00, '2017-01-18 06:58:18', '2017-01-18 06:58:18'),
+	(40, NULL, '0', '9', '8', '7', '6', '5', '4', 0.00, '2017-01-18 07:05:26', '2017-01-18 07:05:26');
+/*!40000 ALTER TABLE `shares` ENABLE KEYS */;
 
 
 -- Dumping structure for table cosociety.surveys
